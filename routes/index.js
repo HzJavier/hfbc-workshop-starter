5dd0b6e83fa62b267adb4bedf1cba8bff33b0cc6
HzJavier <jhernandez@ooyala.com>
2014-04-01 23:02:01 -0600

    Add get for places

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,21 @@
 module.exports = function (app) {
+
+  var places = [
+    {
+      name: 'Home',
+      address: 'Guadalajara'
+    },
+    {
+      name: 'ITESM',
+      address: 'Zapopan'
+    }
+  ];
+
   app.get('/', function (req, res) {
     res.send('Success');
+  });
+
+  app.get('/api/places', function (req, res) {
+    res.send(JSON.stringify(places)); 
   });
 };
