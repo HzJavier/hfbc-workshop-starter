79f2bb64342aa52c9d6f74e6c6000963914fb53f
HzJavier <jhernandez@ooyala.com>
2014-04-01 22:57:53 -0600

    Create routes for api

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var port = 5000;
 var express = require('express'),
-    app = express();
+    app = express(),
+    routes;
+
+app.use(express.logger());
+
+routes = require('./routes')(app);
 
 port = process.env.PORT || 5000;
 app.listen(port, function () {
