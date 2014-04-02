5ed1a06960b6287cfd41f8bcc812bdb913a1ecab
HzJavier <jhernandez@ooyala.com>
2014-04-01 23:50:33 -0600

    Add method to read from local files

---
+++ fileReader.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+exports.readFromFile = function (file, callback) {
+  var buff - new Buffer(5000);
+  fs.open(file, 'r', function (err, fd) {
+    fs.fstat(fd, function (err, stats) {
+      fs.read(fd, buff, 0, 5000, null, callback);
+    });
+  });
+};
